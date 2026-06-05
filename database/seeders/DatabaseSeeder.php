2cf71359e583b535591a501e5f8498ff370f2fc7
Douglas Moreno <dmcinformatica@gmail.com>
2026-06-05 10:07:22 -0300

    FIX - Users Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -23,11 +24,35 @@ final class DatabaseSeeder extends Seeder
             ActivityTypeSeeder::class,
         ]);
 
+        $company = Company::create([
+            'name' => 'SG Tecnologia',
+        ]);
+
         User::create([
             'name' => 'Ariane Melo',
             'email' => 'amelo@sgtecnologia.com.br',
             'password' => bcrypt('101010'),
             'role_id' => 1,
+            'email_verified_at' => now(),
+            'company_id' => $company->id,
+        ]);
+
+        User::create([
+            'name' => 'Andreia Barros',
+            'email' => 'abarros@sgtecnologia.com.br',
+            'password' => bcrypt('101010'),
+            'role_id' => 2,
+            'email_verified_at' => now(),
+            'company_id' => $company->id,
+        ]);
+
+            User::create([
+            'name' => 'Dayana Veronica',
+            'email' => 'dveronica@sgtecnologia.com.br',
+            'password' => bcrypt('101010'),
+            'role_id' => 2,
+            'email_verified_at' => now(),
+            'company_id' => $company->id,
         ]);
     }
 }
